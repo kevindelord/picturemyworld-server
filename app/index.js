05517e6e088a21e28c2658ec51c2869b4a2c1c15
Kevin Delord <delord.kevin@gmail.com>
2017-05-29 00:50:02 +0200

    Move sample files to related folder. Restructure code

--- app/index.js
+++ app/index.js
@@ -1,6 +1,29 @@
 // /app/index.js
 'use strict';
 
-const express = require('./expressManager')
+const express = require('express')
+const bodyParser = require('body-parser')
+const config = require('../config/config')
+const app = express()
 
-express.start()
+// Documentation: https://www.npmjs.com/package/body-parser
+// Use BodyParser to parse the body of a request
+// TODO: urlencoded extended or not?
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Init authentication through Passportjs.
+require('./authentication').init(app)
+
+// Setup basic html views
+require('./views').init(app)
+
+// Setup endpoints.
+require('./users').init(app)
+require('./posts').init(app)
+
+app.listen(config.express.port, (error) => {
+	if (error) {
+		return console.log('something bad happened', error)
+	}
+	console.log(`server is listening on ${config.express.port}`)
+})
