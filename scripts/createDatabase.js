2ed4d4790f914eb5a2d99a80fcecdb023f333ced
Kevin Delord <delord.kevin@gmail.com>
2017-05-28 22:39:07 +0200

    PostgreManager: add new executeQueries functions

--- scripts/createDatabase.js
+++ scripts/createDatabase.js
@@ -2,7 +2,8 @@
 
 const pg = require('pg')
 const config = require('../config/config')
-const exec = require('child_process').exec; 
+const exec = require('child_process').exec;
+const manager = require('../app/postgreManager')
 
 var query = {};
 query.createdb = `CREATE DATABASE ${config.postgre.database}` 
@@ -19,47 +20,22 @@ query.createPostsTable = "CREATE TABLE POSTS(	ID INT PRIMARY KEY NOT NULL,\
 												DATE DATE NOT NULL,\
 												RATIO DECIMAL NOT NULL);"
 
-function executeQuery(client, query, next) {
-	client.query(query, function (error, result) {
-		if (error) {
-			console.log(`ERROR: ${error}`)
-			process.exit(1)
-		} else {
-			next(client)
-		}		
-	})
-}
-
-function connect(url, next) {
-	pg.connect(url, function (error, client, done) {
-		if (error) {
-			console.log(`ERROR: ${error}`)
-			process.exit(1)
-		} else {
-			next(client, done)
-		}
-	})
-}
-
 function createTables(client, done) {
 	console.log("Create 'users' table...")
-	executeQuery(client, query.createUsersTable, function (client) {
-		console.log("Create 'posts' table...")
-		executeQuery(client, query.createPostsTable, function (client) {
-			done()
-			process.exit(0)
-		})
+	manager.executeQueries(client, [query.createUsersTable, query.createPostsTable], 0, function (client) {
+		done()
+		process.exit(0)
 	})
 }
 
 function connectDatabase(next) {
 	console.log("Initial connection to PostgreSQL database...")
-	connect(config.postgre.initURL, function (client, done) {
+	manager.connect(config.postgre.initURL, function (client, done) {
 		console.log("Create new database...")
-		executeQuery(client, query.createdb, function (client) {
+		manager.executeQuery(client, query.createdb, function (client) {
 			done()
 			console.log("Connect to new database...")
-			connect(config.postgre.connectURL, next)
+			manager.connect(config.postgre.connectURL, next)
 		})
 	})
 }
---
+++ scripts/populateDatabase.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+'use strict'
+
+const pg = require('pg')
+const config = require('../config/config')
+const exec = require('child_process').exec; 
+const manager = require('../app/postgreManager')
+
+var query = {};
+
+query.insertUser = "INSERT INTO USERS(ID, USERNAME, EMAIL) VALUES (3, 'John Doe', 'test@gmail.com');"
+
+query.insertPost = "INSERT INTO POSTS(ID, TITLE, DESCRIPTION, LOCATION, LAT, LNG, DATE, RATIO)\
+								VALUES (1, 'Lovely Title', 'That was good', 'London Bridge', 0.34567, -64.4356, '2017-01-08', 0.75);"
+
+
+// Connect to existing database and then insert values.
+console.log("Connect to PostgreSQL database...")
+manager.connect(config.postgre.connectURL, function (client, done) {	
+	const queries = [query.insertUser, query.insertPost]
+	manager.executeQueries(client, queries, 0, function (client) {
+		done()
+		process.exit(0)
+	})
+})
