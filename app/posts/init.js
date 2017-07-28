8e781bf96bb89215ea7a9ab2dda3645303cf24ca
Kevin Delord <delord.kevin@gmail.com>
2017-07-29 00:31:29 +0200

    API: create POST endpoint now takes a required image object

--- app/posts/init.js
+++ app/posts/init.js
@@ -16,37 +16,28 @@ function getPosts(request, response) {
 }
 
 function createPost(request, response) {
-	const post = request.body
-	post.user_id = request.session.passport['user']
-	manager.createPost(post, function (error, result) {
-		if (error) {
-			return response.status(500).json({"status": 500, "message": `ERROR: ${error}`})
-		} else {
-			return response.status(200).json({"status": 200, "message": "New post successfully created"})
-		}
-	})
-}
-
-function createPostWithImage(request, response) {
-	const upload = multer({ dest: 'uploads/' }).single('image')
+	const user_identifier = request.session.passport['user']
+	const upload = multer({ dest: './user_uploads/' }).single('image')
 	upload(request, response, function (error) {
-		// req.file is the `avatar` file.
-		// req.body will hold the text fields, if there were any.
-		console.log(request.file)
-		console.log(request.body)
-
 		if (error) {
 			return response.status(400).json({"status": 400, "message": `${error}`, "code": `${error["code"]}`})
-		} else {
-			return response.status(200).json({"status": 200, "message": "New Image Uploaded"})
 		}
+
+		const post = request.body	// request.body will hold the text fields.
+		const image = request.file 	// request.file is the `image` file.
+		manager.createImagePost(post, image, user_identifier, function (error, result) {
+			if (error) {
+				return response.status(500).json({"status": 500, "message": `${error}`, "code": `${error["code"]}`})
+			} else {
+				return response.status(200).json({"status": 200, "message": "New post successfully created"})
+			}
+		})
 	})
 }
 
 function initPosts (app) {
 	app.get('/posts', passport.authenticationMiddleware(), getPosts)
 	app.post('/post', passport.authenticationMiddleware(), createPost)
-	app.post('/upload', passport.authenticationMiddleware(), createPostWithImage)
 }
 
 module.exports = initPosts
