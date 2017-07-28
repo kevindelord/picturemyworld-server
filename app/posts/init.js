f361f158a80a5804d0e88b3ffc952a44683fa045
Kevin Delord <delord.kevin@gmail.com>
2017-07-28 21:59:42 +0200

    Store image using multer

--- app/posts/init.js
+++ app/posts/init.js
@@ -3,6 +3,7 @@
 
 const manager 	= require('../postgreManager')
 const passport 	= require('passport')
+const multer  = require('multer')
 
 function getPosts(request, response) {
 	manager.getPosts(function (error, result) {
@@ -26,9 +27,26 @@ function createPost(request, response) {
 	})
 }
 
+function createPostWithImage(request, response) {
+	const upload = multer({ dest: 'uploads/' }).single('image')
+	upload(request, response, function (error) {
+		// req.file is the `avatar` file.
+		// req.body will hold the text fields, if there were any.
+		console.log(request.file)
+		console.log(request.body)
+
+		if (error) {
+			return response.status(400).json({"status": 400, "message": `${error}`, "code": `${error["code"]}`})
+		} else {
+			return response.status(200).json({"status": 200, "message": "New Image Uploaded"})
+		}
+	})
+}
+
 function initPosts (app) {
 	app.get('/posts', passport.authenticationMiddleware(), getPosts)
 	app.post('/post', passport.authenticationMiddleware(), createPost)
+	app.post('/upload', passport.authenticationMiddleware(), createPostWithImage)
 }
 
 module.exports = initPosts
