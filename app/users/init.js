459ccc97adc9f08bc6776f1d56887c0f5d4b21ad
Kevin Delord <delord.kevin@gmail.com>
2017-07-31 00:28:19 +0200

    Users: get all users does not required a session. Add more verification on createUser

--- app/users/init.js
+++ app/users/init.js
@@ -8,6 +8,10 @@ const passport 	= require('passport');
 
 function createUser(request, response) {
 	const user = request.body;
+	if (!user.email || !user.password || !user.username) {
+		return response.status(400).json({"status": 400, "message": `ERROR: Missing parameter ${user.email}`});
+	}
+
 	bcrypt.hash(user.password, config.get("bcrypt.seedLength"), function(error, hash) {
 		if (error) {
 			// Pass the error to the express error handler
@@ -30,14 +34,14 @@ function getUsers(request, response) {
 		if (error) {
 			return response.status(500).json({"status": 500, "message": `ERROR: ${error}`});
 		} else {
-			return response.json(result);
+			return response.status(200).json({"status": 200, "users": result});
 		}
 	});
 }
 
 function initUsers (app) {
 	app.post('/users', createUser);
-	app.get('/users', passport.authenticationMiddleware(), getUsers);
+	app.get('/users', getUsers);
 }
 
 module.exports = initUsers;
